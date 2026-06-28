72fae405402f64f315605fcf0e9ad08eac72237d
Jonas John <jonasjohnpookkodan@gmail.com>
2026-06-28 23:47:53 +0530

    new

--- workloads/industrial_safety/inference/backend.py
+++ workloads/industrial_safety/inference/backend.py
@@ -1,9 +1,1 @@
-import platform
-
-if platform.machine() == "aarch64":
-
-    from .tensorrt_backend import infer
-
-else:
-
-    from .opencv_backend import infer
+from .opencv_backend import infer
