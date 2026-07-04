41030ffc14d8da8dd6b35af76de6e40b51b26a63
Jonas John <jonasjohnpookkodan@gmail.com>
2026-07-04 19:12:09 +0530

    jetson fixing

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("best.pt")
+model = YOLO("yolov5nu.pt")
 
 model.export(
     format="onnx",
     opset=11,
     simplify=False,
-    dynamic=False,
     imgsz=640
 )
